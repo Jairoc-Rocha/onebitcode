340a56a2b3b115b4a090b654120d905d850de20e
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-07-28 14:01:18 -0300

    finalizando o modulo 15_EVENTOS

--- JS/15_EVENTOS/js/scripts.js
+++ JS/15_EVENTOS/js/scripts.js
@@ -1,10 +1,10 @@
-// adicionando eventos
+// 1 - adicionando eventos
 const btn = document.querySelector("#my-button");
 btn.addEventListener("click", function () {
   console.log("clicou aqui!");
 });
 
-// removendo eventos
+// 2- removendo eventos
 const secondBtn = document.querySelector("#btn");
 
 function imprimirMensagem() {
@@ -19,3 +19,113 @@ thirdBtn.addEventListener("click", () => {
   console.log("Evento removido");
   secondBtn.removeEventListener("click", imprimirMensagem);
 });
+
+// 3 - argumento do evento
+const myTitle = document.querySelector("#my-title");
+myTitle.addEventListener("click", (event) => {
+  console.log(event);
+  console.log(event.offsetX);
+  console.log(event.pointerType);
+  console.log(event.target);
+});
+
+// 4 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+  console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("Evento 2");
+});
+
+// 5 - removendo avento padrão
+const a = document.querySelector("a");
+
+a.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("Não alterou a página!");
+});
+
+// 6 - eventos do teclado
+// document.addEventListener("keyup", (e) => {
+//   console.log(`Soltou a tecla ${e.key}`);
+// });
+
+// document.addEventListener("keydown", (e) => {
+//   console.log(`Apertou a tecla ${e.key}`);
+// });
+// 7 - eventos de mouse
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () => {
+  console.log("Pressionou o botão");
+});
+
+mouseEvents.addEventListener("mouseup", () => {
+  console.log("Soltou o botão");
+});
+
+mouseEvents.addEventListener("dblclick", () => {
+  console.log("Click duplo");
+});
+
+// 8 - movimento do mouse
+// document.addEventListener("mousemove", (e) => {
+//   console.log(`No eixo x: ${e.x}`);
+//   console.log(`No eixo y: ${e.y}`);
+// });
+
+// 9 - evento de scroll
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 200) {
+    console.log("Passamos de 200px");
+  }
+});
+
+// 10 evento de focus/blur
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", () => {
+  console.log("Entrou no input");
+});
+
+input.addEventListener("blur", () => {
+  console.log("Saiu do input");
+});
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+  console.log("A página carregou!");
+});
+
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  e.returnValue = "mensagem";
+  console.log("Saindo da página");
+});
+
+// 12 - debounce
+const debounce = (f, delay) => {
+  let timeout;
+
+  return (...arguments) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      f.apply(arguments);
+    }, delay);
+  };
+};
+
+window.addEventListener(
+  "mousemove",
+  debounce(() => {
+    console.log("Executando a cada 400ms");
+  }, 400)
+);
