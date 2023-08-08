c8df65906879923f6717d78acfbd406929ab9671
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-08-08 15:29:28 -0300

    iniciando o projeto LAZY_LOAD

---
+++ JS/17_AXIOS/js/scripts.js
@@ -0,0 +1,61 @@
+// 1 - instalação
+console.log(axios);
+
+// 2 - primeiro request
+const getData = async () => {
+  try {
+    const response = await postsFetch.get("/users", {
+      headers: {
+        "Content-Type": "application/json",
+        custom: "header",
+      },
+    });
+
+    console.log(response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+// 3 - imprimindo dados na tela
+const container = document.querySelector("#user-container");
+
+async function printData() {
+  const data = await getData();
+  console.log(data);
+
+  data.map((user) => {
+    const div = document.createElement("div");
+
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = user.name;
+    div.appendChild(nameElement);
+
+    const emailElement = document.createElement("p");
+    emailElement.textContent = user.email;
+    div.appendChild(emailElement);
+
+    container.appendChild(div);
+  });
+}
+
+printData();
+
+// 5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  postsFetch.post("/posts", {
+    title: titleInput.value,
+    body: bodyInput.value,
+    userId: 1,
+  });
+});
